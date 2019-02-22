78f49d97c44e3a0999ada7eb254db909bc7ebb66
Novare Account <Novare@CheshireCat.local>
2019-02-22 10:13:29 +0800

    initial Commit of codes

--- Steph/Main/MainVC.swift
+++ Steph/Main/MainVC.swift
@@ -8,13 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+public final class MainVC: UIViewController {
+    
+    // MARK: Initializers
+    public init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: LifeCycle Methods
+    public override func loadView() {
+        self.view = MainView()
+    }
+    
+    public override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    // MARK: Instance Methods
+    
+    // MARK: Stored Properties
+    
+    // MARK: Computed Properties
+    
 
 }
 
--- Steph/Main/MainView.swift
+++ Steph/Main/MainView.swift
@@ -8,14 +8,17 @@
 
 import UIKit
 
-class MainView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+public final class MainView: UIView {
+    
+    // MARK: Subviews
+    
+    // MARK: Initializers
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
     }
-    */
-
+    
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
