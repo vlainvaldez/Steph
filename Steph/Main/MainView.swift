6b0192819f9fa3880de24fd450f4508df51a4af1
Novare Account <Novare@CheshireCat.local>
2019-02-22 10:12:26 +0800

    Added Ignoring

---
+++ Steph/Main/MainView.swift
@@ -0,0 +1,21 @@
+//
+//  MainView.swift
+//  Steph
+//
+//  Created by Novare Account on 22/02/2019.
+//  Copyright © 2019 Novare Account. All rights reserved.
+//
+
+import UIKit
+
+class MainView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
